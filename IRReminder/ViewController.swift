b78a6a3f4eb6857bad9eb43f2faee68b3a515171
Phil Chang <phil.chang@yahooinc.com>
2022-07-30 18:38:37 +0800

    Initial Commit

---
+++ IRReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IRReminder
+//
+//  Created by Phil Chang on 2022/7/30.
+//  Copyright © 2022 Yahoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
